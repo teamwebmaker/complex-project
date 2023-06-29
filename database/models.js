987ad75f5629b1a638b2188695de1e217fb36f32
webmaker <teamwebmaker@gmail.com>
2023-06-29 15:18:39 +0400

    Created Database Structure

---
+++ database/models.js
@@ -0,0 +1,69 @@
+/**
+ * @typedef { {id: string, title: string } } category
+ */
+
+/**
+ * @typedef { {id: string, email:string, password: string } } user
+ */
+
+/**
+ * @typedef { {id: string, 
+ *              userId: string,
+ *              categoryId: string,
+ *              title: string, 
+ *              description: string, 
+ *              rating: number,
+ *              views: number, 
+ *              category: string
+ *              tags: string[]
+ *            } } post
+ */
+
+/**
+ * @typedef { {id: string, userId: string, postId: string, comment: string } } comment
+ */
+
+/**
+ * @type {category}
+ */
+let category
+
+/**
+ * @type {category[]}
+ */
+let categories
+
+/**
+ * @type {user}
+ */
+let user
+
+/**
+ * @type {user[]}
+ */
+let users
+
+/**
+ * @type {post}
+ */
+let post
+
+/**
+ * @type {post[]}
+ */
+let posts
+
+/**
+ * @type {comment}
+ */
+let comment
+
+/**
+ * @type {comment[]}
+ */
+let comments
+
+
+
+
+
